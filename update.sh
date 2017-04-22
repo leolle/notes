8c7b3bfd39fc857ee192d7776d246dc2332a9707
leolle <victor.wuv@gmail.com>
2017-04-22 20:21:45 +0800

    update notes

--- update.sh
+++ update.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 cp -R -u org/resources/ ./
+cp -R -u ./ipynb/site/fonts ./docs/ipynb/site/
+cp ./ipynb/site/404.html ./ipynb/site/index.html ./ipynb/site/sitemap.xml ~/website/leolle.github.io/docs/ipynb/site
 git add *
 git commit -m "update notes"
 git push
