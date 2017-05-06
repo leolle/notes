10324a8d70d8403665c3a3b04798616bb1e684d8
leolle <victor.wuv@gmail.com>
2017-05-06 15:46:23 +0800

    update notes

--- update.sh
+++ update.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #cp -R -u org/resources/ ./
+rm -rf ./docs/docs/
 cp -R -u ./ipynb/site/fonts ./docs/ipynb/site/
 cp ./ipynb/site/404.html ./ipynb/site/index.html ./ipynb/site/sitemap.xml ~/website/leolle.github.io/docs/ipynb/site
 git add *
