da69082e2412a148c621738a60abeeba81b60133
leolle <victor.wuv@gmail.com>
2017-10-24 17:53:20 +0800

    update notes

--- update.sh
+++ update.sh
@@ -3,6 +3,11 @@ rm -rf ./docs/docs/
 cp -R -u ./ipynb/site/fonts ./docs/ipynb/site/
 cp -R -u ./ipynb/site/ ./docs/ipynb/
 git add *
+
+#if $1
+#then
+ #   git commit -m "$1"
+  #  else
 git commit -m "update notes"
 #git checkout master
 #git merge home
