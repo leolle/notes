252f08121ef418fae7d685e3d8faf024ef50ab75
leolle <victor.wuv@gmail.com>
2017-10-24 18:52:03 +0800

    commit with shell input

--- update.sh
+++ update.sh
@@ -4,12 +4,14 @@ cp -R -u ./ipynb/site/fonts ./docs/ipynb/site/
 cp -R -u ./ipynb/site/ ./docs/ipynb/
 git add *
 
+# commit with shell input
 if [ -z "$*" ]
 then
     git commit -m "update notes"
 else
     git commit -m "$*"
     fi
+
 #git checkout master
 #git merge home
 git push origin master
